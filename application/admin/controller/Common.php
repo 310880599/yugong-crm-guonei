0f430930d588de788d21f33b6a1407da6308b152
licn <licn_0_0@163.com>
2025-06-23 14:24:51 +0800

    邮箱手机号支持多个&去重&样式优化

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -1,40 +1,41 @@
 <?php
+
 namespace app\admin\controller;
+
 use think\Db;
 use think\Controller;
+
 class Common extends Controller
 {
-    protected $mod,$role,$system,$nav,$menudata,$cache_model,$categorys,$module,$moduleid,$adminRules,$HrefId;
+    protected $mod, $role, $system, $nav, $menudata, $cache_model, $categorys, $module, $moduleid, $adminRules, $HrefId;
     public function initialize()
     {
         //判断管理员是否登录
         if (!session('aid')) {
             $this->redirect('admin/login/index');
         }
-        define('MODULE_NAME',strtolower(request()->controller()));
-        define('ACTION_NAME',strtolower(request()->action()));
+        define('MODULE_NAME', strtolower(request()->controller()));
+        define('ACTION_NAME', strtolower(request()->action()));
         //权限管理
         //当前操作权限ID
-        if(session('aid')!=1){
-            $this->HrefId = db('auth_rule')->where('href',MODULE_NAME.'/'.ACTION_NAME)->value('id');
+        if (session('aid') != 1) {
+            $this->HrefId = db('auth_rule')->where('href', MODULE_NAME . '/' . ACTION_NAME)->value('id');
             //当前管理员权限
             $map['a.admin_id'] = session('aid');
-            $rules=Db::table(config('database.prefix').'admin')->alias('a')
-                ->join(config('database.prefix').'auth_group ag','a.group_id = ag.group_id','left')
+            $rules = Db::table(config('database.prefix') . 'admin')->alias('a')
+                ->join(config('database.prefix') . 'auth_group ag', 'a.group_id = ag.group_id', 'left')
                 ->where($map)
                 ->value('ag.rules');
-            $this->adminRules = explode(',',$rules);
-            if($this->HrefId){
-                if(!in_array($this->HrefId,$this->adminRules)){
+            $this->adminRules = explode(',', $rules);
+            if ($this->HrefId) {
+                if (!in_array($this->HrefId, $this->adminRules)) {
                     $this->error('您无此操作权限');
-
-
                 }
             }
         }
-        $this->cache_model=array('Module','AuthRule','Category','Posid','Field','System','cm');
-        foreach($this->cache_model as $r){
-            if(!cache($r)){
+        $this->cache_model = array('Module', 'AuthRule', 'Category', 'Posid', 'Field', 'System', 'cm');
+        foreach ($this->cache_model as $r) {
+            if (!cache($r)) {
                 savecache($r);
             }
         }
@@ -46,7 +47,8 @@ class Common extends Controller
         $this->cm = cache('cm');
     }
     //空操作
-    public function _empty(){
+    public function _empty()
+    {
         return $this->error('空操作，返回上次访问页面中...');
     }
 }
---
+++ update.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS `crm_leads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `crm_leads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `xs_name` varchar(60) DEFAULT NULL COMMENT '线索名称',
+--   `phone_code` varchar(20) NOT NULL DEFAULT 1  COMMENT '国家号',
+  `xs_status` varchar(100) DEFAULT NULL COMMENT '线索_状态',
+  `last_up_records` varchar(200) DEFAULT NULL COMMENT '最新跟进记录',
+  `last_up_time` datetime DEFAULT NULL COMMENT '实际跟进时间',
+  `next_up_time` datetime DEFAULT NULL COMMENT '下次跟进时间  ',
+  `remark` varchar(600) DEFAULT NULL COMMENT '备注',
+  `wechat` varchar(30) DEFAULT NULL COMMENT '微信号',
+  `xs_source` varchar(200) DEFAULT NULL COMMENT '线索_来源',
+  `xs_area` varchar(100) DEFAULT NULL COMMENT '地区来源',
+  `at_user` varchar(100) DEFAULT NULL COMMENT '创建人',
+  `at_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `ut_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `pr_user` varchar(30) DEFAULT NULL COMMENT '负责人',
+  `pr_user_bef` varchar(30) DEFAULT NULL COMMENT '前负责人',
+  `pr_dep` varchar(30) DEFAULT NULL COMMENT '所属部门      不使用 ',
+  `pr_dep_bef` varchar(30) DEFAULT NULL COMMENT '前所属部门   不使用 ',
+  `to_kh_time` datetime DEFAULT NULL COMMENT '转客户时间',
+  `to_gh_time` datetime DEFAULT NULL COMMENT '转公海时间',
+  `pr_gh_type` varchar(200) DEFAULT NULL COMMENT '所属公海',
+  `kh_name` varchar(100) DEFAULT NULL COMMENT '客户名称',
+  `kh_contact` varchar(100) DEFAULT NULL COMMENT '客户联系人',
+  `kh_hangye` varchar(255) DEFAULT NULL COMMENT '行业类别',
+  `kh_rank` varchar(100) DEFAULT NULL COMMENT '客户级别',
+  `kh_status` varchar(100) DEFAULT NULL COMMENT '客户状态',
+  `kh_need` varchar(600) DEFAULT NULL COMMENT '客户需求   不使用',
+  `status` varchar(30) DEFAULT '0' COMMENT '0-线索，1-客户，2-公海，3-删除',
+  `issuccess` int(3) NOT NULL DEFAULT '-1' COMMENT '是否成交 1成交 -1未成交',
+  `kh_username` varchar(100) DEFAULT NULL COMMENT '客户用户名',
+  `ispublic` int(2) DEFAULT '1' COMMENT '1 公共 2 个人',
+  PRIMARY KEY (`id`) USING BTREE,
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='CRM线索单表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `crm_contacts`;
+-- 客户联系方式关系表
+CREATE TABLE `crm_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `leads_id` int(11) DEFAULT NULL COMMENT '线索ID',
+  `contact_type` tinyint(1) DEFAULT NULL COMMENT '联系方式类型（1-手机号/2-邮箱/3-Whatsapp',
+  `contact_extra` varchar(100) DEFAULT NULL COMMENT 'extra',
+  `contact_value` varchar(255) DEFAULT NULL COMMENT '联系方式值',
+  `is_delete` tinyint(1) DEFAULT '0' COMMENT '0-正常/1-删除',
+  `created_at` datetime DEFAULT NULL COMMENT '创建时间',
+   PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uniq_contact` ( `contact_type`, `contact_value`) USING BTREE COMMENT '唯一索引',
+  INDEX `idx_contact_value` (`contact_value`) USING BTREE COMMENT '联系方式值索引'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='客户联系方式关系表';
+
