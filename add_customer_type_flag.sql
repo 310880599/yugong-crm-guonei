44282c4f076736cfe43b74f11d790722c0b09f44
思维导图 <310880599@qq.com>
2025-11-29 14:10:22 +0800

    给财务订单编辑和删除的权限和收款账户的权限

--- add_customer_type_flag.sql
+++
@@ -1,5 +0,0 @@
--- 为 crm_client_order 表添加 customer_type_flag 字段
--- 0 = 公司，1 = 个人
-ALTER TABLE `crm_client_order` 
-ADD COLUMN `customer_type_flag` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '用户属性：0=公司，1=个人' AFTER `cname`;
-
